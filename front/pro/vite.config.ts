98427b233001fd084bceb7a0ccef885bb8b10690
Craig Armel YOUAKOUA TCHOUSSOKNOU <armel@Craigs-MacBook-Air.local>
2025-06-05 17:32:38 +0100

    FIX 2

--- front/pro/vite.config.ts
+++ front/pro/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import env from 'dotenv'
+env.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss() 
   ],
+  base: '/dockyard',
   server: {
     host: '0.0.0.0',
     port: 3000 
